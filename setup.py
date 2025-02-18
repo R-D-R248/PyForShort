5861771bc54e8744eea4ae755bc33c06b7b00f37
R-D-R248 <140945791+R-D-R248@users.noreply.github.com>
2025-02-18 12:13:57 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+import os
+
+# Read the contents of the README.md file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="PyForShort",
-    version="1.0",
+    version="1.0.1",
     description="A package for creating shortcuts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Specify markdown format for README.md
     author="Roshan D Roy",
     author_email="roshandeepuroy@gmail.com",
     url="https://github.com/R-D-R248/PyForShort",
@@ -13,8 +20,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        # Add any specific dependencies for creating shortcuts if needed
-    ],
+    install_requires=[],
     python_requires=">=3.6",
 )
